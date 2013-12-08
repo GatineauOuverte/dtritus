104a2981898ff819b17472482d756e4c570ea873
Alexandre Potvin Latreille <alexandre.potvin.latreille@gmail.com>
2013-12-08 01:33:47 -0500

    - Removed the view filters and delegated the search to the item service when the search term changes.
    = Added a top items view when there's no active search.
    
    - Added a details section when you click an item.
    
    - Removed unused classes and id attributes. We can add them as we need when styling.

--- public/js/controllers/itemCtrl.js
+++ public/js/controllers/itemCtrl.js
@@ -1,21 +1,27 @@
-/*global dtritus*/
+/*global angular, dtritus*/
 
 (function () {
     'use strict';
     
     dtritus.controller('ItemCtrl', function ItemCtrl($scope, itemSvc) {
-           
-        $scope.items = [];
         
-        itemSvc.getAll()
-            .success(function (items) {
-                $scope.items = items;
-            })
-            .error(function () {
-                //TODO: Handle failure
-            });
+        $scope.searchResults = [];
+        $scope.mostPopularItems = [];
+        
+        $scope.selectedItem = null;
     
         $scope.searchTerm = '';
+        
+        //Perform search when searchTerm changes
+        $scope.$watch('searchTerm', function () {
+            var term = $scope.searchTerm;
+            
+            if (term) {
+                itemSvc.search(term).then(function (items) {
+                    $scope.searchResults = items;
+                });
+            }
+        });
     
         $scope.sortOptions = [
             { name: 'Catégorie', predicate: ['categoryWd', 'descriptorWd'] },
@@ -25,6 +31,19 @@
         ];
         
         $scope.sortPredicate = $scope.sortOptions[0];
+        
+        $scope.showDetailsFor = function (item) {
+            $scope.selectedItem = item;
+        };
+        
+        $scope.returnToSearchResults = function () {
+            $scope.selectedItem = null;
+        };
+        
+        //Get top 5 popular items
+        itemSvc.getMostPopular(5).then(function (items) {
+            $scope.mostPopularItems = items;
+        });
         
     });
 }());
--- public/js/services/itemSvc.js
+++ public/js/services/itemSvc.js
@@ -3,14 +3,24 @@
 (function () {
     'use strict';
     
-    dtritus.factory('itemSvc', function ($http) {
+    dtritus.factory('itemSvc', function ($http, $filter) {
+        
+        var allItems = $http.get('data/dtritus.json');
         
         return {
-            getAll: function () {
-                return $http.get('data/dtritus.json');
+            search: function (query) {
+                return allItems.then(function (result) {
+                    return $filter('filter')(result.data, query);
+                });
+            },
+            
+            getMostPopular: function (top) {
+                //TODO: Implement most popular algorithm
+                return allItems.then(function (result) {
+                    return result.data.slice(0, top);
+                });
             }
         };
-        
     });
     
 }());
