73d5cfee95bf064ef49c7283d3149f8fe4774edf
Daniel Lauzon <daniel.lauzon@gmail.com>
2013-12-10 17:11:41 -0500

    small style corrections, and use a metro button instead of link

--- js/controllers/itemDetailsCtrl.js
+++ js/controllers/itemDetailsCtrl.js
@@ -3,12 +3,16 @@
 (function () {
     'use strict';
     
-    dtritus.controller('ItemDetailsCtrl', function ItemDetailsCtrl($scope, $routeParams, itemSvc) {
+    dtritus.controller('ItemDetailsCtrl', function ItemDetailsCtrl($scope, $routeParams, itemSvc, $location) {
         $scope.item = null;
         
         itemSvc.findById($routeParams.itemId).then(function (item) {
             $scope.item = item;
         });
+
+        $scope.goBack = function(){
+          $location.path('#/');
+        };
         
     });
 }());
